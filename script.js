b3acd1d7fd449f7f340932acf892fac19c3abbe2
harsh mahajan <127186841+HarshMN2345@users.noreply.github.com>
2023-04-05 00:43:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+const setLectureName = (id, time) => {
+    const element = document.getElementById(id);
+    element.addEventListener('click', () => {
+      const lectureName = prompt(`Enter the name of the lecture at ${time}`);
+      element.innerText = lectureName;
+    });
+  }
+  
+  setLectureName('a', '7-8');
+  setLectureName('b', '8-9');
+  setLectureName('c', '9-10');
+  setLectureName('d', '10-11');
+  setLectureName('e', '11-12');
+  
+  const setLectureName2 = (id, time) => {
+    const element = document.getElementById(id);
+    element.addEventListener('click', () => {
+      const lectureName = prompt(`Enter the name of the lecture at ${time}`);
+      element.innerText = lectureName;
+    });
+  }
+  
+  setLectureName2('f', '7-8');
+  setLectureName2('g', '8-9');
+  setLectureName2('h', '9-10');
+  setLectureName2('i', '10-11');
+  setLectureName2('j', '11-12');
+  
+  const setLectureName3 = (id, time) => {
+    const element = document.getElementById(id);
+    element.addEventListener('click', () => {
+      const lectureName = prompt(`Enter the name of the lecture at ${time}`);
+      element.innerText = lectureName;
+    });
+  }
+  
+  setLectureName3('k', '7-8');
+  setLectureName3('l', '8-9');
+  setLectureName3('m', '9-10');
+  setLectureName3('n', '10-11');
+  setLectureName3('o', '11-12');
+  
+  const setLectureName4 = (id, time) => {
+    const element = document.getElementById(id);
+    element.addEventListener('click', () => {
+      const lectureName = prompt(`Enter the name of the lecture at ${time}`);
+      element.innerText = lectureName;
+    });
+  }
+  
+  setLectureName4('p', '7-8');
+  setLectureName4('q', '8-9');
+  setLectureName4('r', '9-10');
+  setLectureName4('s', '10-11');
+  setLectureName4('u', '11-12');
+  
+  const setLectureName5 = (id, time) => {
+    const element = document.getElementById(id);
+    element.addEventListener('click', () => {
+      const lectureName = prompt(`Enter the name of the lecture at ${time}`);
+      element.innerText = lectureName;
+    });
+  }
+  
+  setLectureName4('v', '7-8');
+  setLectureName4('w', '8-9');
+  setLectureName4('x', '9-10');
+  setLectureName4('y', '10-11');
+  setLectureName4('z', '11-12');
+  
+  const setLectureName6 = (id, time) => {
+    const element = document.getElementById(id);
+    element.addEventListener('click', () => {
+      const lectureName = prompt(`Enter the name of the lecture at ${time}`);
+      element.innerText = lectureName;
+    });
+  }
+  
+  setLectureName6('a1', '7-8');
+  setLectureName6('a2', '8-9');
+  setLectureName6('a3', '9-10');
+  setLectureName6('a4', '10-11');
+  setLectureName6('a5', '11-12');
